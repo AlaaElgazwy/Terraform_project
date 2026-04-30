98686fe66ca9c8a10a637a9df03ea8eb0b7a0ca5
Alaa Elgazwy <ai6949347@gmail.com>
2026-04-30 08:25:14 +0300

    create security group allow ssh from 0.0.0.0/0 and antheor allow ssh and port 3000 from vpc cicr only and create ec2 in pupblic supnet with sg1 and create ec2 in private supnet with sg2

---
+++ modules/compute/main.tf
@@ -0,0 +1,91 @@
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical ID
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+
+resource "aws_security_group" "bastion_sg" {
+  name        = "${var.environment}-bastion-sg"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_security_group" "app_sg" {
+  name        = "${var.environment}-app-sg"
+  description = "Allow SSH and Port 3000 from VPC CIDR only"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
+  }
+
+  ingress {
+    description = "Port 3000 from VPC"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_instance" "bastion" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  subnet_id              = var.public_subnet_id
+  vpc_security_group_ids = [aws_security_group.bastion_sg.id]
+
+  tags = {
+    Name = "${var.environment}-Bastion-EC2"
+  }
+
+  
+  provisioner "local-exec" {
+    command = "echo ${var.environment} Bastion Public IP: ${self.public_ip} >> bastion_ips.txt"
+  }
+}
+
+
+resource "aws_instance" "application" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  subnet_id              = var.private_subnet_id
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
+
+  tags = {
+    Name = "${var.environment}-App-EC2"
+  }
+}
---
+++ modules/compute/variables.tf
@@ -0,0 +1,5 @@
+variable "vpc_id" {}
+variable "vpc_cidr" {}
+variable "public_subnet_id" {}
+variable "private_subnet_id" {}
+variable "environment" {}
